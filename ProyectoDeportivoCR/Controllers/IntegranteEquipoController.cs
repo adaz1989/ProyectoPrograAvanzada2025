af5973313d6addcf131d1ce2f6cc4ae6b241e2b4
Sebastián Segura <ssegura30126@ufide.ac.cr>
2025-03-27 01:55:22 -0600

    Registrar IntegranteTorneo Web

---
+++ ProyectoDeportivoCR/Controllers/IntegranteEquipoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace ProyectoDeportivoCR.Controllers
+{
+    public class IntegranteEquipoController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public IntegranteEquipoController(IHttpClientFactory httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult RegistrarIntegranteEquipo(long torneoId)
+        {
+            var model = new IntegranteEquipoModel
+            {
+                EquipoId = torneoId
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult RegistrarIntegranteEquipo(IntegranteEquipoModel model)
+        {
+            using (var http = _httpClient.CreateClient())
+            {
+                var url = _configuration.GetSection("Variables:urlWebApi").Value + "IntegranteEquipo/RegistrarIntegranteEquipo";
+
+                // http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("Token"));
+                var response = http.PostAsJsonAsync(url, model).Result;
+
+                if (response.IsSuccessStatusCode)
+                    return RedirectToAction("ConsultarTorneos", "Torneos");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ ProyectoDeportivoCR/Models/IntegranteEquipoModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoDeportivoCR.Models
+{
+    public class IntegranteEquipoModel
+    {
+        public long EquipoId { get; set; }
+        public int Rol { get; set; }
+
+        [StringLength(09)]
+        public string? Cedula { get; set; }
+
+        public DateOnly FechaInscripcion { get; set; }
+
+    }
+}
