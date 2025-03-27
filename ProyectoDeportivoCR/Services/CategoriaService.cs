aa677a2af2340d84fe6bf79f3d841f4ccfead07d
Josue Loria <jmiranda90400@ufide.ac.cr>
2025-03-26 21:29:05 -0600

    Llamado a Categoria del Api a el proyecto MVC

---
+++ ProyectoDeportivoCR/Services/CategoriaService.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.Json;
+using ProyectoDeportivoCR.Services.Extensions;
+
+namespace ProyectoDeportivoCR.Services
+{
+    public class CategoriaService : ICategoriaService
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaService(ICategoriaRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public async Task<Respuesta2Model<CategoriaModel>> RegistrarCategoria(CategoriaModel model)
+        {
+            var respuesta = await _categoriaRepository.RegistrarCategoria(model);
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                return await respuesta.LeerRespuesta2Model<CategoriaModel>();
+            }
+
+            return new Respuesta2Model<CategoriaModel>
+            {
+                Exito = false,
+                Mensaje = "Error al comunicarse con la API."
+            };
+        }
+
+        public async Task<Respuesta2Model<CategoriaModel>> ActualizarCategoria(CategoriaModel model)
+        {
+            var respuesta = await _categoriaRepository.ActualizarCategoria(model);
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                return await respuesta.LeerRespuesta2Model<CategoriaModel>();
+            }
+
+            return new Respuesta2Model<CategoriaModel>
+            {
+                Exito = false,
+                Mensaje = "Error al comunicarse con la API."
+            };
+        }
+
+        public async Task<Respuesta2Model<CategoriaModel>> ObtenerCategorias(int categoriaId)
+        {
+            var respuesta = await _categoriaRepository.ObtenerCategorias(categoriaId);
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                return await respuesta.LeerRespuesta2Model<CategoriaModel>();
+            }
+
+            return new Respuesta2Model<CategoriaModel>
+            {
+                Exito = false,
+                Mensaje = "Error al comunicarse con la API."
+            };
+        }
+
+        public async Task<Respuesta2Model<CategoriaModel>> DesabilitarCategoria(int categoriaId)
+        {
+            var respuesta = await _categoriaRepository.DesabilitarCategoria(categoriaId);
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                return await respuesta.LeerRespuesta2Model<CategoriaModel>();
+            }
+
+            return new Respuesta2Model<CategoriaModel>
+            {
+                Exito = false,
+                Mensaje = "Error al comunicarse con la API."
+            };
+        }
+    }
+}
